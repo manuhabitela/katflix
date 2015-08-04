63173aded2f19044439a52c4b2c2abcbfcca4971
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2015-08-04 16:24:23 +0200

    do not use global peerflix bin - use the local one

--- src/peerflix.js
+++ src/peerflix.js
@@ -1,4 +1,6 @@
 var spawn = require('child_process').spawn;
+var path = require('path');
+var execSync = require('child_process').execSync;
 var defaults = require('lodash.defaults');
 var parseArgs = require('./parse-args.js');
 module.exports = {};
@@ -13,11 +15,21 @@ module.exports.play = function play(url, peerflixArguments, options) {
 
 function parseOptions(options) {
     return defaults(options || {}, {
-        bin: "peerflix"
+        bin: getPeerflixBin()
     });
 }
 
 function parsePeerflixArgs(options) {
     return parseArgs.makeString(options).split(' ');
 }
+
+function getPeerflixBin() {
+    var binPath;
+    try {
+        binPath = execSync('npm bin', { encoding: 'utf8' }).trim();
+    } catch (e) {
+        console.log("Error when getting peerflix executable: " + e.message);
+        return false;
+    }
+    return path.join(binPath, 'peerflix');
 }
